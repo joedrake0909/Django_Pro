a60553286adb6a68661ae940fd28bfc108e553c5
Joseph_Bio <josephbiossey03@gmail.com>
2026-09-14 02:28:10 +0000

    Add cart overview page, iterable cart support, and total calculation

--- E-comerce_Website/mysite/cart/cart.py
+++ E-comerce_Website/mysite/cart/cart.py
@@ -1,3 +1,6 @@
+from myapp.models import Product
+from decimal import Decimal
+
 class Cart():
     def __init__(self,request):
         self.session = request.session 
@@ -8,6 +11,25 @@ class Cart():
 
     def __len__(self):
         return sum ( int(item['qty']) for item in self.cart.values() )
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product']=product
+
+        for item in cart.values():
+            item['price']=Decimal(item['price'])
+            item['qty']=Decimal(item['qty'])
+            item['total']=item['price']*item['qty']
+            yield item
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * Decimal(item['qty']) for item in self.cart.values())
+
+
 
 
     def add(self,product,product_qty):

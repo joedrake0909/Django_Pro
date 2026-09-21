23d0742fce5de3573759c269679884509295c7eb
Joseph_Bio <josephbiossey03@gmail.com>
2026-09-21 23:34:10 +0000

    feat: create order and order item functionality

--- E-comerce_Website/mysite/orders/admin.py
+++ E-comerce_Website/mysite/orders/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Address
+from models import Address, Order, OrderItem
 
 admin.site.register(Address)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
+
 
--- E-comerce_Website/mysite/orders/models.py
+++ E-comerce_Website/mysite/orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from myapp.models import Product
 
 class Address(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,5 +12,20 @@ class Address(models.Model):
     state = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=20)
     country = models.CharField(max_length=100)
-    
 
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    is_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
